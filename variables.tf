14544a944ed2dacb1a3d43f0e177a523ab4b1f0f
Venu Bharadwaj <venualonea1@gmail.com>
2021-09-28 18:42:35 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,3 +16,17 @@ variable "subnet_name" {
 variable "snet_address_prefixes" {
   type = list(string)
 }
+
+
+#New Way of Executing Variables
+
+
+variable "virtual_network" {
+  type = object({
+    vnet_name = string
+    address_space=list(string)
+    resource_group_name=list(string)
+    subnet_name=string
+    address_prefixes= list(string)
+  })
+}
