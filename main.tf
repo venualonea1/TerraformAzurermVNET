ea78fd90bc55ca9255bbaf8e65357f42747d887b
Venu Gopal K <venugopal331@gmail.com>
2021-09-28 18:51:49 +0530

    Adding the changes

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 data "azurerm_resource_group" "this" {
-  name = var.resource_group_name
+  name = var.virtual_network.resource_group_name
 }
 
 resource "azurerm_virtual_network" "this" {
   location = data.azurerm_resource_group.this.location
-  name = var.vnet_name
-  address_space = var.vnet_address_space
+  name = var.virtual_network.vnet_name
+  address_space = var.virtual_network.address_space
   resource_group_name = data.azurerm_resource_group.this.name
 }
 resource "azurerm_subnet" "this" {
-  name = var.subnet_name
+  name = var.virtual_network.subnet_name
   resource_group_name =data.azurerm_resource_group.this.name
   virtual_network_name =azurerm_virtual_network.this.name
-  address_prefixes =var.snet_address_prefixes
+  address_prefixes =var.virtual_network.address_prefixes
 
 
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,11 @@
-variable "vnet_location" {
-  type = string
-}
-variable "vnet_name" {
-  type = string
-}
-variable "vnet_address_space" {
-  type = list(string)
-}
-variable "resource_group_name" {
-  type = string
-}
-variable "subnet_name" {
-  type = string
-}
-variable "snet_address_prefixes" {
-  type = list(string)
+
+
+variable "virtual_network" {
+  type = object({
+    vnet_name = string
+    address_space=list(string)
+    resource_group_name=list(string)
+    subnet_name=string
+    address_prefixes= list(string)
+  })
 }
