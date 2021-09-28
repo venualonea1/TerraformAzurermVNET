bf7b91b2afde8ee683cc70ac38c3e21188d94204
Venu Gopal K <venugopal331@gmail.com>
2021-09-28 21:10:30 +0530

    Adding the changes

--- variables.tf
+++ variables.tf
@@ -1,12 +1,10 @@
-
-
 variable "virtual_network" {
   type = object({
     vnet_name = string
     address_space=list(string)
-    resource_group_name=list(string)
     subnet_name=string
     address_prefixes= list(string)
   })
-
 }
+
+variable "resource_group_name" {}
