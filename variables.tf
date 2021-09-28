de644725b4d8b089263725f3d267ae00f4151596
Venu Bharadwaj <venualonea1@gmail.com>
2021-09-28 18:52:37 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,3 @@
-variable "vnet_location" {
-  type = string
-}
-variable "vnet_name" {
-  type = string
-}
-variable "vnet_address_space" {
-  type = list(string)
-}
-variable "resource_group_name" {
-  type = string
-}
-variable "subnet_name" {
-  type = string
-}
-variable "snet_address_prefixes" {
-  type = list(string)
-}
-
-
-#New Way of Executing Variables
 
 
 variable "virtual_network" {
